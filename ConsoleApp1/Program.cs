e2fb7808edc4cf99e84ae60e48aa41c7aeb9f385
Vishesh <pilotvishesh@gmail.com>
2020-08-28 10:50:10 +0530

    DATATYPE CONVERSION

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,7 +10,28 @@ namespace ConsoleApp1
     class Program
     {
         static void Main(string[] args)
+        // Datatype Conversion
         {
+            int i = 100;
+            float f = i;
+            Console.WriteLine(f);
+            float g = 100282821234222.43F;
+            int h = (int)g;
+            
+            Console.WriteLine(h);
+
+            // Parse and Try Parse
+            string s = "100";
+            int V = int.Parse(s);
+            Console.WriteLine(V);
+
+            string TRYPARSE = "100AV";
+            int result = 0;
+            int.TryParse(TRYPARSE, out result );
+            Console.WriteLine(TRYPARSE);
+
+
         }
+       
     }
 }
