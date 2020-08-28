389b4d57ee9a51ee8a02215fa5975b15378032f0
Vishesh <pilotvishesh@gmail.com>
2020-08-28 11:30:15 +0530

    Arrays

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,29 +9,42 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
-        // Datatype Conversion
+         static void Main(string[] args)
+          //Datatype Conversion
+         {
+             int i = 100;
+             float f = i;
+             Console.WriteLine(f);
+             float g = 100282821234222.43F;
+             int h = (int)g;
+
+             Console.WriteLine(h);
+
+              //Parse and Try Parse
+             string s = "100";
+             int V = int.Parse(s);
+             Console.WriteLine(V);
+
+             string TRYPARSE = "100AV";
+             int result = 0;
+             int.TryParse(TRYPARSE, out result );
+             Console.WriteLine(TRYPARSE);
+             Main1();
+         }
+        static void Main1()
+        // Arrays
         {
-            int i = 100;
-            float f = i;
-            Console.WriteLine(f);
-            float g = 100282821234222.43F;
-            int h = (int)g;
-            
-            Console.WriteLine(h);
+            int[] Evennumbers = new int[5];
 
-            // Parse and Try Parse
-            string s = "100";
-            int V = int.Parse(s);
-            Console.WriteLine(V);
-
-            string TRYPARSE = "100AV";
-            int result = 0;
-            int.TryParse(TRYPARSE, out result );
-            Console.WriteLine(TRYPARSE);
+            Evennumbers[0] = 0;
+            Evennumbers[1] = 2;
+            Evennumbers[2] = 4;
+            Evennumbers[3] = 6;
+            Evennumbers[4] = 8;
 
 
+            Console.WriteLine(Evennumbers[4]);
         }
-       
+
     }
 }
