a64375ffed1a84df574098c0c90db96878822fa4
Vishesh <pilotvishesh@gmail.com>
2020-09-07 17:38:22 +0530

    IF statement, Switch statement, And ICECREAM PROGRAM

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -46,5 +46,100 @@ namespace ConsoleApp1
             Console.WriteLine(Evennumbers[4]);
         }
 
+        static void Main()
+        //IF STATEMENT
+        {
+              Console.Write("Please Enter Number = ");
+              int Usernumber = Convert.ToInt32(Console.ReadLine());
+
+              if (Usernumber == 1)
+              {
+                  Console.WriteLine("Your Number is 1");
+              }
+             else if (Usernumber == 2)
+              {
+                  Console.WriteLine("Your Number is 2");
+              }
+              else if (Usernumber == 3)
+              {
+                  Console.WriteLine("Your Number is 3");
+              }
+              else 
+              {
+                  Console.WriteLine("YOUR NUMBER IS NOT IN 1 TO 3");
+              }
+
+              //Switch Statement
+
+              Console.Write("Please Enter A Number = ");
+              int UserNumber = Convert.ToInt32(Console.ReadLine());    
+
+              switch(UserNumber)
+              {
+                  case 1:
+                      Console.WriteLine("Your Number is 1");
+                      break;
+                  case 2:
+                      Console.WriteLine("Your Number is 2");
+                      break;
+                  case 3:
+                      Console.WriteLine("Your Number is 3");
+                      break;
+                  case 4:
+                      Console.WriteLine("Your Number is 4");
+                      break;
+                  default:
+                      Console.WriteLine("Your Number is not 1,2,3,4");
+                      break;
+              }
+            //Program With Switch statement
+            // ICE CREAM Shop Programming with Switch and Goto 
+
+            int TotalIcecreamCost = 0;
+            Retry:
+            Console.WriteLine("Please Select Coffee Flavour From Menu");
+            Console.WriteLine("1 Chocolate");
+            Console.WriteLine("2 Vanilla");
+            Console.WriteLine("3 Rajbhog");
+            string CustomerChoice = Console.ReadLine();
+
+            switch(CustomerChoice)
+            {
+                case "1":
+                    TotalIcecreamCost += 30;
+                    break;
+                case "2":
+                    TotalIcecreamCost += 20;
+                    break;
+                case "3":
+                    TotalIcecreamCost += 40;
+                    break;
+                default :
+                    Console.WriteLine("Your Choice {0} is Invalid", CustomerChoice);
+                    goto Retry;
+                    
+            }
+            Error:
+            Console.WriteLine("Do you Want To Purchase More");
+            Console.WriteLine("Yes or No");
+            string CustomerDesicion = Console.ReadLine();
+
+            switch(CustomerDesicion.ToUpper())
+            {
+                case "YES"://while converting to upper case (yes) should be in upper case and vice versa
+                    goto Retry;
+                case "NO":
+                    break;
+                default:
+                    Console.WriteLine("Your choice {0} is invalid", CustomerDesicion);
+                    goto Error;
+            }
+
+
+            Console.WriteLine("Bill amount = {0}", TotalIcecreamCost);
+            Console.WriteLine("Thanks For Shopping ");
+        }
+
+        
     }
 }
